2f64abb7a14872fdf2063fd7c1d16694b013ef16
Mehmet Yıldırım <mehmetyildirim.developer@gmail.com>
2022-10-26 14:26:59 +0300

    fix: game loop 2.0

--- src/main/Game.java
+++ src/main/Game.java
@@ -7,7 +7,7 @@ public class Game implements Runnable {
 	private Thread gameThread;
 	private final int FPS_SET = 120;
 	private final int UPS_SET = 200;
-	
+
 
 	public Game() {
 
@@ -23,32 +23,56 @@ public class Game implements Runnable {
 		gameThread.start();
 	}
 
-//	@Override
-	public void run() {
-
-		double timePerFrame = 1000000000.0 / FPS_SET;
-		long lastFrame = System.nanoTime();
-		long now = System.nanoTime();
-
-		int frames = 0;
-		long lastCheck = System.currentTimeMillis();
-
-		while (true) {
-
-			now = System.nanoTime();
-			if (now - lastFrame >= timePerFrame) {
-				gamePanel.repaint();
-				lastFrame = now;
-				frames++;
-			}
-
-			if (System.currentTimeMillis() - lastCheck >= 1000) {
-				lastCheck = System.currentTimeMillis();
-				System.out.println("FPS: " + frames);
-				frames = 0;
-			}
-		}
+	private void update() {
+		gamePanel.updateGame();
 
 	}
+	//	@Override
+	public void run() {//her pcde ayni hizda calismasi icin ayarliyoruz
 
+
+		double timePerFrame = 1000000000.0 / FPS_SET;
+		double timePerUpdate = 1000000000.0 / UPS_SET;
+		long previousTime = System.nanoTime();
+
+		int frames = 0;
+		int updates= 0;
+		long lastCheck = System.currentTimeMillis();
+
+		double deltaU =0;//update
+		double deltaF =0;//fps
+
+		while (true) {
+			long currentTime= System.nanoTime();
+
+			deltaU +=(currentTime - previousTime) / timePerUpdate;
+			deltaF +=(currentTime - previousTime) / timePerFrame;
+
+			previousTime = currentTime;	
+
+			if (deltaU >= 1) {
+				update();
+				updates++;
+				deltaU--;
+			}
+
+			if (deltaF >=1) {
+				gamePanel.repaint();
+				frames++;
+				deltaF--;
+			}
+
+
+
+			if (System.currentTimeMillis() - lastCheck >= 1000) 
+			{
+				lastCheck = System.currentTimeMillis();
+				System.out.println("FPS: " + frames + "| UPS: " + updates );
+				frames = 0;
+				updates = 0;
+			}
+		}
+	}
 }
+
+
